e625f1cdde8b71147f7f1e0d6393b698d4565b23
fabianbogado <fabicrack218@gmail.com>
2022-04-19 20:18:55 -0300

    cambios

---
+++ header.php
@@ -0,0 +1,31 @@
+<header class="container">
+    <?php include_once("header.php") ?>
+        <nav class="navbar navbar-expand-md  mb-4">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link px-4 py-1" href="index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-4 py-1" href="sobre-mi.php">Sobre mí</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-4 py-1" href="proyecto.php">Proyectos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-4 py-1 active" href="contacto.php">Contacto</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex">
+                        <a class="btn btn-outline-success" type="submit">Descargar mi CV <i
+                                class="fa-solid fa-download"></i></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
--- proyecto.php
+++ proyecto.php
@@ -15,38 +15,9 @@
 
 <body id="proyectos">
 
-    <header class="container">
-        <nav class="navbar navbar-expand-md  mb-4">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    <i class="fa-solid fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="index.php">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="sobre-mi.php">Sobre mí</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1 active" href="proyecto.php">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="contacto.php">Contacto</a>
-                        </li>
-                    </ul>
-                    <div class="d-flex">
-                        <a class="btn btn-outline-success" type="submit">Descargar mi CV <i
-                                class="fa-solid fa-download"></i></a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-    </header>
+   
+    <?php include_once("header.php") ?>
+  
     <main class="container">
         <div class="row">
             <div class="col-12 mt-3 mb-5">
