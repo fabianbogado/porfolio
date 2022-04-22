277c3f3c89b2f6d5ea4d60eebb5d607a7f7994dd
fabianbogado <fabicrack218@gmail.com>
2022-04-22 17:33:40 -0300

    cambios

--- sobre-mi.php
+++ sobre-mi.php
@@ -30,15 +30,195 @@ $pg = "sobre-mi";
             </div>
                     </div>
                     <div class="col-sm-3 col-12 mt-4 offset-sm-2">
-                        <img src="imagenes/fabian-bogado.jpg" alt="Fabian bogado" title="Fabian bogado"class="img-fluid img-circle" style=
-                                                                                                                                        "border-radius: 50%;    margin: 1em auto;
-                                                                                                                                        display: block;
-                                                                                                                                        background-color: #fff;
-                                                                                                                                        padding: 10px;">
-                    </div> <!--no me tomaba el css-->
-
+                        <img src="imagenes/fabian-bogado.jpg" alt="Fabian bogado" title="Fabian bogado"class="img-fluid img-circle"</div> <!--no me tomaba el css-->
         </div>
-
+                </div>
+            </div>
+        </div>
+        <section id="tecnologias" class="color-gradiente pb-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="my-sm-5 text-white">Stack tecnológico</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>Javascript</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://img.stackshare.io/service/1209/javascript.jpeg">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>PHP</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://img.stackshare.io/service/991/hwUcGZ41_400x400.jpg">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>HTML5</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://img.stackshare.io/service/2538/kEpgHiC9.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>React.js</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://img.stackshare.io/service/1020/OYIaJ1KK.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>jQuery</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://img.stackshare.io/service/1021/lxEKmMnB_400x400.jpg">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>Bootstrap</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://img.stackshare.io/service/1101/C9QJ7V3X.png">
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-sm-4 mt-0">
+                    <div class="col-sm-2 col-6">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>Laravel</h3>
+                            <img class="img-fluid d-block mx-auto" width="90"
+                                src="https://depcsuite.com/wp-content/uploads/2019/08/220px-Laravel.svg_.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>MySQL</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://img.stackshare.io/service/1615/mariadb-logo-400x400.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>CSS</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://img.stackshare.io/service/6727/css.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>Git</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://img.stackshare.io/service/1046/git.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>Apache</h3>
+                            <img class="img-fluid d-block mx-auto" width="110"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Apache_Software_Foundation_Logo_%282016%29.svg/1200px-Apache_Software_Foundation_Logo_%282016%29.svg.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-6 text-center">
+                        <div class="px-4 py-5 card mx-0 mb-4 mb-sm-0">
+                            <h3>Mercadopago</h3>
+                            <img class="img-fluid d-block mx-auto" width="60"
+                                src="https://img.stackshare.io/service/12698/gUjuxmmd_400x400.jpg">
+                        </div>
+                    </div>
+                </div>
+        </section>
+            </div>
+        <section id="experiencia" class="container">
+            <div class="row">
+                <div class="col-12 pt-5 pb-4">
+                    <h2><i class="fas fa-briefcase"></i> Experiencia laboral</h2>
+                </div>
+            </div>
+            <div class="row shadow bg-white rounded">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-2 p-5 my-auto d-none d-sm-block">
+                            <img src="imagenes/sin-logo.png" class="img-fluid baw" title="sin-logo">
+                        </div>
+                        <div class="col-12- col-sm-10 p-3">
+                            <h3>Atencion/Reposioción en un minimercado</h3>
+                            <h4>2021-2022</h4>
+                            <p>Repocitor y atencion al cliente en un minimercado del barrio durante las vacaciones</p>
+                        </div>
+                    </div>
+                </div>   
+            </div>
+        </section>             
+        <section id="formacion" class="container">
+            <div class="row">
+                <div class="col-12 pt-5 pb-4">
+                    <h2><i class="fas fa-graduation-cap"></i> Formación acádémica</h2>
+                </div>
+            </div>
+            <div class="row shadow bg-white rounded p-1">
+                <div class="col-12 col-sm-6">
+                    <div class="row">
+                        <div class="col-2 py-3 my-auto d-none d-sm-block">
+                            <img src="imagenes/sin-logo.png" class="img-fluid baw" title="sin-logo">
+                        </div>
+                        <div class="col-12- col-sm-10 p-3">
+                            <h3>Formacion Secundaria</h3>
+                            <h4>Escuela secundaria N15 "Frida kahlo"</h4>
+                            <h5>2018 – 2021</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6">
+                    <div class="row">
+                        <div class="col-2 py-3 my-auto d-none d-sm-block">
+                            <img src="imagenes/sin-logo.png" class="img-fluid baw" title="sin-logo">
+                        </div>
+                        <div class="col-12- col-sm-10 p-3">
+                            <h3>Conputacion/Informatica basica</h3>
+                            <h4>England</h4>
+                            <h5>2019 – 2021</h5>
+                            <p> HIghlan <br></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="idiomas">
+            <div class="container">
+                <div class="row py-5 mx-0">
+                    <div class="col-sm-6 col-12 pb-3 pb-sm-0">
+                        <div class="row mr-sm-1 bg-white card-idioma shadow me-sm-1">
+                            <div class="col-4 text-center card">
+                                <i class="fas fa-comment-alt"></i>
+                            </div>
+                            <div class="col-8 p-5">
+                                <h2>IDIOMAS</h2>
+                                <ul>
+                                    <li>ESPANOL</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-12">
+                        <div class="row ml-sm-2 bg-white card-idioma shadow ms-sm-1">
+                            <div class="col-4 text-center card">
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <div class="col-8 p-5">
+                                <h2>HOBBIES</h2>
+                                <ul>
+                                    <li>FUTBOl</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>  
     </main>
     <footer class="container mt-5">
         <div class="row">
